0ca27bce59bc08503c2d46eab20c658f6765342b
aoualiaymen <aymen1.aouali@gmail.com>
2025-09-05 11:14:36 +0100

    first release

---
+++ st7796.h
@@ -0,0 +1,243 @@
+/*
+ * st7796.h
+ *
+ *  Created on: Aug 27, 2024
+ *      Author: ENIGMA
+ */
+
+#ifndef INC_ST7796_H_
+#define INC_ST7796_H_
+
+//#include "fonts.h"
+#include "string.h"
+
+#define DMA_BUFFER_SIZE 			64U
+#define BURST_MAX_SIZE 	750
+
+#define ILI9488_TFTWIDTH  320
+#define ILI9488_TFTHEIGHT 480
+
+#define ILI9488_NOP     0x00
+#define ILI9488_SWRESET 0x01
+#define ILI9488_RDDID   0x04
+#define ILI9488_RDDST   0x09
+
+#define ILI9488_SLPIN   0x10
+#define ILI9488_SLPOUT  0x11
+#define ILI9488_PTLON   0x12
+#define ILI9488_NORON   0x13
+
+#define ILI9488_RDMODE  0x0A
+#define ILI9488_RDMADCTL  0x0B
+#define ILI9488_RDPIXFMT  0x0C
+#define ILI9488_RDIMGFMT  0x0D
+#define ILI9488_RDSELFDIAG  0x0F
+
+#define ILI9488_INVOFF  0x20
+#define ILI9488_INVON   0x21
+#define ILI9488_GAMMASET 0x26
+#define ILI9488_DISPOFF 0x28
+#define ILI9488_DISPON  0x29
+
+#define ILI9488_CASET   0x2A
+#define ILI9488_PASET   0x2B
+#define ILI9488_RAMWR   0x2C
+#define ILI9488_RAMRD   0x2E
+
+#define ILI9488_PTLAR   0x30
+#define ILI9488_MADCTL  0x36
+#define ILI9488_PIXFMT  0x3A
+
+#define ILI9488_FRMCTR1 0xB1
+#define ILI9488_FRMCTR2 0xB2
+#define ILI9488_FRMCTR3 0xB3
+#define ILI9488_INVCTR  0xB4
+#define ILI9488_DFUNCTR 0xB6
+
+#define ILI9488_PWCTR1  0xC0
+#define ILI9488_PWCTR2  0xC1
+#define ILI9488_PWCTR3  0xC2
+#define ILI9488_PWCTR4  0xC3
+#define ILI9488_PWCTR5  0xC4
+#define ILI9488_VMCTR1  0xC5
+#define ILI9488_VMCTR2  0xC7
+
+#define ILI9488_RDID1   0xDA
+#define ILI9488_RDID2   0xDB
+#define ILI9488_RDID3   0xDC
+#define ILI9488_RDID4   0xDD
+
+#define ILI9488_GMCTRP1 0xE0
+#define ILI9488_GMCTRN1 0xE1
+/*
+#define ILI9488_PWCTR6  0xFC
+
+*/
+
+// Color definitions
+#define ILI9488_BLACK      			0x0000      /*   0,   0,   0 */
+#define ILI9488_NAVY				0x000F      /*   0,   0, 128 */
+#define ILI9488_DARKGREEN   		0x03E0      /*   0, 128,   0 */
+#define ILI9488_DARKCYAN    		0x03EF      /*   0, 128, 128 */
+#define ILI9488_MAROON      		0x7800      /* 128,   0,   0 */
+#define ILI9488_PURPLE      		0x780F      /* 128,   0, 128 */
+#define ILI9488_OLIVE       			0x7BE0      /* 128, 128,   0 */
+#define ILI9488_LIGHTGREY   		0xC618      /* 192, 192, 192 */
+#define ILI9488_DARKGREY    		0x7BEF      /* 128, 128, 128 */
+#define ILI9488_BLUE        			0x001F      /*   0,   0, 255 */
+#define ILI9488_GREEN       		0x07E0      /*   0, 255,   0 */
+#define ILI9488_CYAN        			0x07FF      /*   0, 255, 255 */
+#define ILI9488_RED         			0xF800      /* 255,   0,   0 */
+#define ILI9488_MAGENTA     		0xF81F      /* 255,   0, 255 */
+#define ILI9488_YELLOW      		0xFFE0      /* 255, 255,   0 */
+#define ILI9488_WHITE       			0xFFFF      /* 255, 255, 255 */
+#define ILI9488_ORANGE      		0xFD20      /* 255, 165,   0 */
+#define ILI9488_GREENYELLOW 	0xAFE5      /* 173, 255,  47 */
+#define ILI9488_PINK        			0xF81F
+
+typedef uint16_t colour_t;
+
+// colour bit layout rrrr rggg gggb bbbb
+#define BLACK 						((colour_t)0x0000)
+#define YELLOW 						((colour_t)0xffe0)
+#define RED 						((colour_t)0xf800)
+#define GREEN 						((colour_t)0x07e0)
+#define BLUE 						((colour_t)0x001f)
+#define WHITE 						((colour_t)0xffff)
+#define PINK 						((colour_t)0xf80e)
+#define PURPLE 						((colour_t)0xf83f)
+#define	GREY15						((colour_t)0x1082)
+#define	GREY14						((colour_t)0x2104)
+#define	GREY13						((colour_t)0x3186)
+#define	GREY12						((colour_t)0x4208)
+#define	GREY11						((colour_t)0x528a)
+#define	GREY10						((colour_t)0x630c)
+#define	GREY9						((colour_t)0x738e)
+#define	GREY8						((colour_t)0x8410)
+#define	GREY7						((colour_t)0x9492)
+#define	GREY6						((colour_t)0xa514)
+#define	GREY5						((colour_t)0xb596)
+#define	GREY4						((colour_t)0xc618)
+#define	GREY3						((colour_t)0xd69a)
+#define	GREY2						((colour_t)0xe71c)
+#define	GREY1						((colour_t)0xf79e)
+#define ORANGE 						((colour_t)0xfb80)
+#define CYAN						((colour_t)0x07ff)
+#define DARK_CYAN					((colour_t)0x0492)
+#define LIGHT_ORANGE				((colour_t)0xfe20)
+#define BRICK_RED					((colour_t)0xb104)
+
+#define MADCTL_MY  0x80
+#define MADCTL_MX  0x40
+#define MADCTL_MV  0x20
+#define MADCTL_ML  0x10
+#define MADCTL_RGB 0x00
+#define MADCTL_BGR 0x08
+#define MADCTL_MH  0x04
+
+#define ST7735_MADCTL_MY  0x80
+#define ST7735_MADCTL_MX  0x40
+#define ST7735_MADCTL_MV  0x20
+#define ST7735_MADCTL_ML  0x10
+#define ST7735_MADCTL_RGB 0x00
+#define ST7735_MADCTL_BGR 0x08
+#define ST7735_MADCTL_MH  0x04
+
+#define ST7735_IS_160X80 1
+#define ST7735_XSTART 1
+#define ST7735_YSTART 26
+#define ST7735_WIDTH  480
+#define ST7735_HEIGHT 320
+#define ST7735_ROTATION (ST7735_MADCTL_MX | ST7735_MADCTL_MV | ST7735_MADCTL_RGB)
+
+#define ST7735_NOP     0x00
+#define ST7735_SWRESET 0x01
+#define ST7735_RDDID   0x04
+#define ST7735_RDDST   0x09
+
+#define ST7735_SLPIN   0x10
+#define ST7735_SLPOUT  0x11
+#define ST7735_PTLON   0x12
+#define ST7735_NORON   0x13
+
+#define ST7735_INVOFF  0x20
+#define ST7735_INVON   0x21
+#define ST7735_GAMSET  0x26
+#define ST7735_DISPOFF 0x28
+#define ST7735_DISPON  0x29
+#define ST7735_CASET   0x2A
+#define ST7735_RASET   0x2B
+#define ST7735_RAMWR   0x2C
+#define ST7735_RAMRD   0x2E
+
+#define ST7735_PTLAR   0x30
+#define ST7735_COLMOD  0x3A
+#define ST7735_MADCTL  0x36
+#define ST7796_IDMOFF 0x38
+
+#define ST7796_IFMODE 0xB0
+#define ST7735_FRMCTR1 0xB1
+#define ST7735_FRMCTR2 0xB2
+#define ST7735_FRMCTR3 0xB3
+#define ST7735_INVCTR  0xB4
+#define ST7735_DISSET5 0xB6
+
+#define ST7735_PWCTR1  0xC0
+#define ST7735_PWCTR2  0xC1
+#define ST7735_PWCTR3  0xC2
+#define ST7735_PWCTR4  0xC3
+#define ST7735_PWCTR5  0xC4
+#define ST7735_VMCTR1  0xC5
+
+#define ST7735_RDID1   0xDA
+#define ST7735_RDID2   0xDB
+#define ST7735_RDID3   0xDC
+#define ST7735_RDID4   0xDD
+
+#define ST7735_PWCTR6  0xFC
+
+#define ST7735_GMCTRP1 0xE0
+#define ST7735_GMCTRN1 0xE1
+#define ST7796_DOCA 0xE8
+
+// Color definitions
+#define	ST7735_BLACK   0x0000
+#define	ST7735_BLUE    0x001F
+#define	ST7735_RED     0xF800
+#define	ST7735_GREEN   0x07E0
+#define ST7735_CYAN    0x07FF
+#define ST7735_MAGENTA 0xF81F
+#define ST7735_YELLOW  0xFFE0
+#define ST7735_WHITE   0xFFFF
+#define ST7735_COLOR565(r, g, b) (((r & 0xF8) << 8) | ((g & 0xFC) << 3) | ((b & 0xF8) >> 3))
+
+typedef enum {
+	GAMMA_10 = 0x01,
+	GAMMA_25 = 0x02,
+	GAMMA_22 = 0x04,
+	GAMMA_18 = 0x08
+} GammaDef;
+
+void st7796_init();
+void set_window_color(uint16_t xs, uint16_t xe, uint16_t ys, uint16_t ye, uint16_t color);
+//void print_fonts_char(sFONT font,uint16_t startX,uint16_t startY,char a,uint16_t color,uint16_t bg_color);
+//void write_string(sFONT font,char a[],uint16_t startX,uint16_t startY,uint16_t color,uint16_t bg_color);
+void V_line(uint16_t startX,uint16_t startY,uint16_t lenght,uint16_t width,uint16_t color);
+void H_line(uint16_t startX,uint16_t startY,uint16_t lenght,uint16_t width,uint16_t color);
+void print_image( uint8_t* image,uint16_t xs, uint16_t xe, uint16_t ys, uint16_t ye);
+void cs_clear();
+void print_dma(uint8_t* image);
+void print_image_DMA(uint8_t* image,uint16_t xs, uint16_t xe, uint16_t ys, uint16_t ye);
+void write_char(uint8_t font_size,uint16_t x,uint16_t y, char ch, uint16_t bach_ground_color,uint16_t char_color);
+void set_window_area(uint16_t xs, uint16_t xe, uint16_t ys, uint16_t ye);
+void st7796_command(uint8_t cmd);
+void st7796_write_data(uint8_t* data, uint32_t size);
+
+
+/*void ILI9488_Init(void);
+void setAddrWindow(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1);
+void ILI9341_Fill_Screen(uint16_t Colour);
+void ILI9341_Draw_Colour(uint16_t Colour);
+void setRotation(uint8_t r);*/
+
+#endif /* INC_ST7796_H_ */
